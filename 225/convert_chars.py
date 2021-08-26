6c4173df1fbe2ac77a51dcfcc126a3f858eb2a09
Craig Richards <craig@greywidget.co.uk>
2021-08-26 09:40:54 +0100

    submission Bite 225 @ codechalleng.es

--- 225/convert_chars.py
+++ 225/convert_chars.py
@@ -6,3 +6,7 @@ def convert_pybites_chars(text):
     Return the resulting string."""
 
     return "".join(c.swapcase() if c.lower() in PYBITES else c for c in text)
+
+
+if __name__ == "__main__":
+    convert_pybites_chars("kEnsentme")
