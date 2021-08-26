9d3a5e4d1624533a6b05c8605de4f83279439a63
Craig Richards <craig@greywidget.co.uk>
2021-08-26 09:49:42 +0100

    submission Bite 225 @ codechalleng.es

--- 225/convert_chars.py
+++ 225/convert_chars.py
@@ -5,8 +5,6 @@ def convert_pybites_chars(text):
     """Swap case all characters in the word pybites for the given text.
     Return the resulting string."""
 
-    return "".join(c.swapcase() if c.lower() in PYBITES else c for c in text)
-
-
-if __name__ == "__main__":
-    convert_pybites_chars("kEnsentme")
+    return "".join(
+        char.swapcase() if char.lower() in PYBITES else char for char in text
+    )
