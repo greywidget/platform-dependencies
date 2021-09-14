e5591001f24b7e0c58d28b8ff9713f2a733aa516
Craig Richards <craig@greywidget.co.uk>
2021-09-14 21:26:39 +0100

    submission Bite 295 @ codechalleng.es

--- 295/join_lists.py
+++ 295/join_lists.py
@@ -1,4 +1,5 @@
 from typing import List, Union
+from functools import reduce
 
 
 def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
@@ -7,9 +8,27 @@ def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
 
     result = []
     for count, lst in enumerate(lst_of_lst, start=1):
-        if lst:
-            for item in lst:
-                result.append(item)
-            if count < len(lst_of_lst):
-                result.append(sep)
+        for item in lst:
+            result.append(item)
+        if count < len(lst_of_lst):
+            result.append(sep)
     return result
+
+
+# def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
+#     if not lst_of_lst:
+#         return None
+
+#     return reduce(lambda x, y: x + [sep] + y, lst_of_lst)
+
+
+# def join_lists(lst_of_lst: List[List[str]], sep: str) -> Union[List[str], None]:
+#     def joiner(lst_of_lst, sep):
+#         for idx, lst in enumerate(lst_of_lst):
+#             yield from lst
+#             if idx < len(lst_of_lst) - 1:
+#                 yield sep
+
+#     if not lst_of_lst:
+#         return None
+#     return list(joiner(lst_of_lst, sep))
