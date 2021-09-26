e821e19e6b780b3d39cb5a0dc0687b2b2787a006
Craig Richards <craig@greywidget.co.uk>
2021-09-26 16:47:11 +0100

    submission Bite 10 @ codechalleng.es

---
+++ 10/divide.py
@@ -0,0 +1,12 @@
+def positive_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+    except ZeroDivisionError:
+        result = 0
+    except TypeError as e:
+        raise e
+
+    if result < 0:
+        raise ValueError
+
+    return result
