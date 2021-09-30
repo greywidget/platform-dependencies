805cc9f8e97c3d4b7053be14c819c4202fbb8e38
Craig Richards <craig@greywidget.co.uk>
2021-09-30 10:57:49 +0100

    bite 17 add default parms

--- 17/friends.py
+++ 17/friends.py
@@ -1,6 +1,6 @@
 from itertools import combinations, permutations
 
 
-def friends_teams(friends, team_size, order_does_matter):
+def friends_teams(friends, team_size=2, order_does_matter=False):
     func = permutations if order_does_matter else combinations
     return func(friends, team_size)
