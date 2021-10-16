850389b77721b9c9d2c445c3c1afa9ec9bef4243
Craig Richards <craig@greywidget.co.uk>
2021-10-16 09:30:04 +0100

    submission Bite 59 @ codechalleng.es

--- 59/tables.py
+++ 59/tables.py
@@ -4,34 +4,24 @@ class MultiplicationTable:
     def __init__(self, length):
         """Create a 2D self._table of (x, y) coordinates and
            their calculations (form of caching)"""
-        self._table = {
-            (x, y): x * y for x in range(1, length + 1)
-            for y in range(1, length + 1)
-        }
+        self._table = [
+            [x * y for y in range(1, length + 1)]
+            for x in range(1, length + 1)
+        ]
 
     def __len__(self):
         """Returns the area of the table (len x* len y)"""
-        return len(self._table)
+        return len(self._table) ** 2
 
     def __str__(self):
         """Returns a string representation of the table"""
-        vals = [str(value) for value in self._table.values()]
-        length = int(sqrt(len(self._table)))
-        start, end = 0, length
         diagram = ""
-        for _ in range(length):
-            diagram += " | ".join(vals[start:end]) + "\n"
-            start += length
-            end += length
+        for row in self._table:
+            alphas = [str(item) for item in row]
+            diagram += " | ".join(alphas) + "\n"
+            
         return diagram
 
     def calc_cell(self, x, y):
         """Takes x and y coords and returns the re-calculated result"""
-        result = self._table.get((x, y)) 
-        if result:
-            return result
-        raise IndexError("Invalid coordinates")
-
-if __name__ == "__main__":
-    table = MultiplicationTable(3)
-    print(table)
+        return self._table[x-1][y-1]
