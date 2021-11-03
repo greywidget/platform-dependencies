31359e356929f2b5f7ca7adf1439d20ecfdac167
Craig Richards <craig@greywidget.co.uk>
2021-11-03 21:09:10 +0000

    submission Bite 95 @ codechalleng.es

---
+++ 95/bdaydict.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+MSG = "Hey {}, there are more people with your birthday!"
+
+
+class BirthdayDict(dict):
+    """Override dict to print a message every time a new person is added that has
+    the same birthday (day+month) as somebody already in the dict"""
+
+    def __setitem__(self, name: str, birthday: date):
+        if (birthday.month, birthday.day) in [
+            (item.month, item.day) for item in self.values()
+        ]:
+            print(MSG.format(name))
+        dict.__setitem__(self, name, birthday)
