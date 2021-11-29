7283a4b96f6bdba55f888fc26fd889fd11eba81e
Craig Richards <craig@greywidget.co.uk>
2021-11-29 21:18:23 +0000

    submission Bite 116 @ codechalleng.es

--- 116/files.py
+++ 116/files.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 ONE_KB = 1024
@@ -7,8 +6,6 @@ ONE_KB = 1024
 def get_files(dirname, size_in_kb):
     """Return files in dirname that are >= size_in_kb"""
 
-    return [
-        item.name
-        for item in Path(dirname).glob("*")
-        if item.is_file and ((item.stat().st_size / ONE_KB) >= size_in_kb)
-    ]
+    for item in Path(dirname).glob("*"):
+        if item.is_file() and item.stat().st_size >= size_in_kb * ONE_KB:
+            yield item.name
