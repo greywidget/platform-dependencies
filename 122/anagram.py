57eda0b27938e9c303021dc3c9a23138322990aa
Craig Richards <craig@greywidget.co.uk>
2022-01-23 15:32:29 +0000

    Bite 122 added some extra stuff

--- 122/anagram.py
+++ 122/anagram.py
@@ -5,3 +5,18 @@ def is_anagram(word1, word2):
     return sorted(word1.replace(" ", "").lower()) == sorted(
         word2.replace(" ", "").lower()
     )
+
+
+# That works, but here is the PyBites solution
+# Also an interesting thing I picked up reading the forum is:
+# str.casefold()
+from collections import Counter
+
+
+def _clean_word(word):
+    return word.strip().replace(" ", "").lower()
+
+
+def is_anagram2(word1, word2):
+    word1, word2 = _clean_word(word1), _clean_word(word2)
+    return Counter(word1) == Counter(word2)
