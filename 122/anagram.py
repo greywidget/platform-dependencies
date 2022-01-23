55f11a58ebb901bc8254fd63ac35c4c86c070ea9
Craig Richards <craig@greywidget.co.uk>
2022-01-23 15:13:14 +0000

    submission Bite 122 @ codechalleng.es

---
+++ 122/anagram.py
@@ -0,0 +1,7 @@
+def is_anagram(word1, word2):
+    """Receives two words and returns True/False (boolean) if word2 is
+    an anagram of word1, ignore case and spacing.
+    About anagrams: https://en.wikipedia.org/wiki/Anagram"""
+    return sorted(word1.replace(" ", "").lower()) == sorted(
+        word2.replace(" ", "").lower()
+    )
