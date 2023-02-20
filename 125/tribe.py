51fdc8107599e64456e5ee770fd937be0f96073d
Craig Richards <craig@greywidget.co.uk>
2023-02-20 21:18:45 +0000

    submission Bite 125 @ codechalleng.es

---
+++ 125/tribe.py
@@ -0,0 +1,45 @@
+from collections import Counter
+
+from bs4 import BeautifulSoup as Soup
+import requests
+
+AMAZON = "amazon.com"
+# static copy
+TIM_BLOG = "https://bites-data.s3.us-east-2.amazonaws.com/" "tribe-mentors-books.html"
+MIN_COUNT = 3
+
+
+def load_page():
+    """Download the blog html and return its decoded content"""
+    with requests.Session() as session:
+        return session.get(TIM_BLOG).content.decode("utf-8")
+
+
+def get_top_books(content=None):
+    """Make a BeautifulSoup object loading in content,
+    find all links that contain AMAZON, extract the book title
+    (stripping spacing characters), and count them.
+    Return a list of (title, count) tuples where
+    count is at least MIN_COUNT
+    """
+    if content is None:
+        content = load_page()
+
+    soup = Soup(content, "html.parser")
+
+    titles = [link.string for link in soup.find_all("a") if AMAZON in link["href"]]
+
+    counter = Counter(titles)
+    return [
+        (text.strip() if text else None, count)
+        for text, count in counter.items()
+        if count >= MIN_COUNT
+    ]
+
+
+def main():
+    print(get_top_books())
+
+
+if __name__ == "__main__":
+    main()
