3f6bd163ac975d7db33b41e185ced0f2298b7ac3
Craig Richards <craig@greywidget.co.uk>
2023-03-20 17:46:34 +0000

    submission Bite 138 @ codechalleng.es

--- 138/zoo.py
+++ 138/zoo.py
@@ -1,17 +1,18 @@
+import itertools
+
+
 class Animal:
+    _seq = itertools.count(10001)
+    _zoo = []
 
-    _counter = 10000
-    animals = []
-
-    def __init__(self, name: str):
-        self.name = name.capitalize()
-        Animal._counter += 1
-        self.counter = Animal._counter
-        Animal.animals.append(self)
+    def __init__(self, name):
+        self.id = next(self._seq)
+        self.name = name.title()
+        self._zoo.append(self)
 
     def __str__(self):
-        return f"{self.counter}. {self.name}"
+        return f"{self.id}. {self.name}"
 
     @classmethod
     def zoo(cls):
-        return "\n".join([str(animal) for animal in Animal.animals])
+        return "\n".join([str(animal) for animal in cls._zoo])
