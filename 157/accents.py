8882dd60464acfda557c4d478af7ff06296b985a
Craig Richards <craig@greywidget.co.uk>
2023-03-31 11:38:56 +0100

    submission Bite 157 @ codechalleng.es

---
+++ 157/accents.py
@@ -0,0 +1,18 @@
+from unicodedata import decomposition
+
+
+def filter_accents(text):
+    """Return a sequence of accented characters found in
+    the passed in lowercased text string
+    """
+    return sorted(letter for letter in text.lower() if decomposition(letter))
+
+
+text = (
+    "The 5 French accents;"
+    "The cédille (cedilla) Ç ..."
+    "The accent aigu (acute accent) é ..."
+    "The accent circonflexe (circumflex) â, ê, î, ô, û ..."
+    "The accent grave (grave accent) à, è, ù ..."
+    "The accent tréma (dieresis/umlaut) ë, ï, ü"
+)
