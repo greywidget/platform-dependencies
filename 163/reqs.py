0dcc8d694b730da2a26ce5c090d13e299555b731
Craig Richards <craig@greywidget.co.uk>
2023-04-03 23:25:09 +0100

    submission Bite 163 @ codechalleng.es

--- 163/reqs.py
+++ 163/reqs.py
@@ -37,3 +37,33 @@ def changed_dependencies(old_reqs: str, new_reqs: str) -> List[str]:
             new_deps.append(k)
 
     return new_deps
+
+
+# PYBITES SOLUTION
+# ================
+# from distutils.version import StrictVersion
+# from typing import Dict, List
+
+
+# def _get_package_dict(reqs: str) -> Dict[str, str]:
+#     """Helper to parse requirements str into a dict of
+#        (package,  version) k, v pairs
+#     """
+#     return dict(line.split('==') for line  # type: ignore
+#                 in reqs.strip().splitlines())
+
+
+# def changed_dependencies(old_reqs: str, new_reqs: str) -> List[str]:
+#     """Compare old vs new requirement multiline strings
+#        and return a list of dependencies that have been upgraded
+#        (have a newer version)
+#     """
+#     old = _get_package_dict(old_reqs)
+#     new = _get_package_dict(new_reqs)
+
+#     dependencies = []
+#     for package, old_version in old.items():
+#         new_version = new.get(package)
+#         if StrictVersion(new_version) > StrictVersion(old_version):
+#             dependencies.append(package)
+#     return dependencies
