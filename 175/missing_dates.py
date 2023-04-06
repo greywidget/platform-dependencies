b1c731ac205efbbe1d34b8a8e5d3d1bb8d3c6c99
Craig Richards <craig@greywidget.co.uk>
2023-04-06 15:19:02 +0100

    submission Bite 175 @ codechalleng.es

--- 175/missing_dates.py
+++ 175/missing_dates.py
@@ -1,10 +1,25 @@
+from datetime import date, timedelta
+
+
 def get_missing_dates(dates):
     """Receives a range of dates and returns a sequence
-       of missing datetime.date objects (no worries about order).
+    of missing datetime.date objects (no worries about order).
 
-       You can assume that the first and last date of the
-       range is always present (assumption made in tests).
+    You can assume that the first and last date of the
+    range is always present (assumption made in tests).
 
-       See the Bite description and tests for example outputs.
+    See the Bite description and tests for example outputs.
     """
-    pass
+
+    dates.sort()
+    date_from = dates[0]
+    a_day = timedelta(days=1)
+    missing_dates = []
+    a_date = date_from
+
+    for item in dates:
+        while item != a_date:
+            missing_dates.append(a_date)
+            a_date += a_day
+        a_date += a_day
+    return missing_dates
