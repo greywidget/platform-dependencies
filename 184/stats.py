95d5d699c151ca4faf3d9f50e83ea5158bbba7da
Craig Richards <craig@greywidget.co.uk>
2023-04-10 14:44:48 +0100

    submission Bite 184 @ codechalleng.es

--- 184/stats.py
+++ 184/stats.py
@@ -1,49 +1,60 @@
-from csv import DictReader
 import os
+from collections import Counter
+from csv import DictReader
 from urllib.request import urlretrieve
 
 TMP = os.getenv("TMP", "/tmp")
-LOGS = 'bite_output_log.txt'
+LOGS = "bite_output_log.txt"
 DATA = os.path.join(TMP, LOGS)
-S3 = 'https://bites-data.s3.us-east-2.amazonaws.com'
+S3 = "https://bites-data.s3.us-east-2.amazonaws.com"
 if not os.path.isfile(DATA):
-    urlretrieve(f'{S3}/{LOGS}', DATA)
+    urlretrieve(f"{S3}/{LOGS}", DATA)
 
 
 class BiteStats:
-
     def __init__(self, data=DATA):
         self.rows = []
-        # you code ...
+        with open(data) as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader:
+                self.rows.append(row)
 
     @property
     def number_bites_accessed(self) -> int:
         """Get the number of unique Bites accessed"""
-        pass
+        return len(set(item["bite"] for item in self.rows))
 
     @property
     def number_bites_resolved(self) -> int:
         """Get the number of unique Bites resolved (completed=True)"""
-        pass
+        return len(
+            set(item["bite"] for item in self.rows if item["completed"] == "True")
+        )
 
     @property
     def number_users_active(self) -> int:
         """Get the number of unique users in the data set"""
-        pass
+        return len(set(item["user"] for item in self.rows))
 
     @property
     def number_users_solving_bites(self) -> int:
         """Get the number of unique users that resolved
-           one or more Bites"""
-        pass
+        one or more Bites"""
+        return len(
+            set(item["user"] for item in self.rows if item["completed"] == "True")
+        )
 
     @property
     def top_bite_by_number_of_clicks(self) -> str:
         """Get the Bite that got accessed the most
-           (= in most rows)"""
-        pass
+        (= in most rows)"""
+        counter = Counter(item["bite"] for item in self.rows)
+        return int(counter.most_common(1)[0][0])
 
     @property
     def top_user_by_bites_completed(self) -> str:
         """Get the user that completed the most Bites"""
-        pass
+        counter = Counter(
+            item["user"] for item in self.rows if item["completed"] == "True"
+        )
+        return counter.most_common(1)[0][0]
