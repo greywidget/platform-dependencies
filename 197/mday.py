f961a8c5316e7c48806ad3dd5062e31ff73b7f05
Craig Richards <craig@greywidget.co.uk>
2023-04-17 16:46:04 +0100

    Added additional comments/solutions for bite 197

--- 197/mday.py
+++ 197/mday.py
@@ -21,3 +21,13 @@ def get_mothers_day_date(year):
 #        is celebrated assuming it's the 2nd Sunday of May."""
 #     first_of_may = date(year=year, month=MAY, day=1)
 #     return first_of_may + relativedelta(weeks=1, weekday=SU)
+
+# ==================================================================
+# Also another similar solution was
+#
+# from dateutil.rrule import rrule, SU, MONTHLY
+#
+# def get_mothers_day_date(year):
+#     """Given the passed in year int, return the date Mother's Day
+#        is celebrated assuming it's the 2nd Sunday of May."""
+#     return rrule(MONTHLY, byweekday=SU(2), dtstart=date(year, 5, 1))[0].date()
