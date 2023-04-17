97c3d466c6e0887118c5563a8865f05685734bc5
Craig Richards <craig@greywidget.co.uk>
2023-04-17 16:38:08 +0100

    submission Bite 197 @ codechalleng.es

--- 197/mday.py
+++ 197/mday.py
@@ -1,7 +1,10 @@
 from datetime import date
+from dateutil.rrule import rrule, WEEKLY
 
 
 def get_mothers_day_date(year):
     """Given the passed in year int, return the date Mother's Day
-       is celebrated assuming it's the 2nd Sunday of May."""
-    pass
+    is celebrated assuming it's the 2nd Sunday of May."""
+    start_date = date(year, 5, 1)
+
+    return rrule(dtstart=start_date, freq=WEEKLY, count=2, byweekday=6)[1].date()
