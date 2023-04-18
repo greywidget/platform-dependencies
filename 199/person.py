87d3c99a5133d599a6742c910c91bb1f343ef0ab
Craig Richards <craig@greywidget.co.uk>
2023-04-18 18:13:01 +0100

    submission Bite 199 @ codechalleng.es - simplified

--- 199/person.py
+++ 199/person.py
@@ -1,30 +1,18 @@
 class Person:
-    def __init__(self):
-        pass
-
     def __str__(self):
         return "I am a person"
 
 
 class Father(Person):
-    def __init__(self):
-        super()
-
     def __str__(self):
         return f"{super().__str__()} and cool daddy"
 
 
 class Mother(Person):
-    def __init__(self):
-        super()
-
     def __str__(self):
         return f"{super().__str__()} and awesome mom"
 
 
-class Child(Father, Mother, Person):
-    def __init__(self):
-        super()
-
+class Child(Father, Mother):
     def __str__(self):
         return "I am the coolest kid"
