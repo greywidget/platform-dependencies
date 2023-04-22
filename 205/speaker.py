6fbfcef06e8133315f7e0c2b92f3c373f8bd3ed0
Craig Richards <craig@greywidget.co.uk>
2023-04-22 18:01:35 +0100

    Bite 205 show PyBites solution

--- 205/speaker.py
+++ 205/speaker.py
@@ -1,11 +1,10 @@
 from urllib.request import urlretrieve
 import os
 from pathlib import Path
-from itertools import chain
+import re
 
 import gender_guesser.detector as gender
 from bs4 import BeautifulSoup as Soup
-from collections import Counter
 
 TMP = Path(os.getenv("TMP", "/tmp"))
 PYCON_HTML = TMP / "pycon2019.html"
@@ -19,35 +18,46 @@ def _get_soup(html=PYCON_HTML):
     return Soup(html.read_text(encoding="utf-8"), "html.parser")
 
 
+def _extract_names(names):
+    for name in names:
+        extracted_names = re.split(r"[,/]", name)
+        for en in extracted_names:
+            yield en.strip()
+
+
+def _get_first_names(names):
+    return [n.split()[0] for n in names]
+
+
 def get_pycon_speaker_first_names(soup=None):
     """Parse the PYCON_HTML using BeautifulSoup, extracting all
     speakers (class "speaker"). Note that some items contain
     multiple speakers so you need to extract them.
     Return a list of first names
     """
-    if not soup:
+    if soup is None:
         soup = _get_soup()
 
-    speakers = soup.find_all("span", "speaker")
-    names = list(
-        chain(*[item.string.strip().replace("/", ",").split(",") for item in speakers])
-    )
-    first_names = [name.strip().split(" ")[0] for name in names]
-    return first_names
+    speakers = soup.find_all("span", {"class": "speaker"})
+    names = [speaker.text.strip() for speaker in speakers]
+
+    names = list(_extract_names(names))
+    return _get_first_names(names)
 
 
 def get_percentage_of_female_speakers(first_names):
     """Run gender_guesser on the names returning a percentage
     of female speakers (female and mostly_female),
     rounded to 2 decimal places."""
-    guesser = gender.Detector()
-    gender_count = Counter(guesser.get_gender(name) for name in first_names)
-    return round(
-        (gender_count["female"] + gender_count["mostly_female"])
-        / len(first_names)
-        * 100,
-        2,
-    )
+    d = gender.Detector()
+    num_females = 0
+
+    for name in first_names:
+        gg = d.get_gender(name)
+        if "female" in gg:
+            num_females += 1
+
+    return round(num_females / len(first_names) * 100, 2)
 
 
 if __name__ == "__main__":
