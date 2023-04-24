7cb54b384ac0c97f43d9d5605c2a71b93ca2a5a2
Craig Richards <craig@greywidget.co.uk>
2023-04-24 14:04:45 +0100

    submission Bite 212 @ codechalleng.es

--- 212/summing.py
+++ 212/summing.py
@@ -10,6 +10,5 @@ def sum_numbers(numbers):
     Task: use contextlib's suppress twice to make the code below more concise.
     """
     for i, j in zip(numbers, numbers[1:]):
-        with suppress(TypeError):
-            with suppress(ZeroDivisionError):
-                yield i / j
+        with suppress(TypeError, ZeroDivisionError):
+            yield i / j
