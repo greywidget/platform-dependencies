2bc29aabb91cf50dec4ccb1a3ffae3538c8651d9
Craig Richards <craig@greywidget.co.uk>
2023-04-26 10:25:16 +0100

    Modify Bite 219 to show better solution

--- 219/notifications.py
+++ 219/notifications.py
@@ -4,13 +4,8 @@ TODAY = date.today()
 
 
 def gen_bite_planning(num_bites=1, num_days=1, start_date=TODAY):
-    delta_days = timedelta(days=num_days)
-    bite_date = start_date + delta_days
-    bite_count = 0
-
+    day = 0
     while True:
-        if bite_count == num_bites:
-            bite_count = 0
-            bite_date += delta_days
-        yield bite_date
-        bite_count += 1
+        day += num_days
+        for _ in range(num_bites):
+            yield start_date + timedelta(days=day)
