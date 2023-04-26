74f3946efc2fb6891923e53cbf56949eff9c8942
Craig Richards <craig@greywidget.co.uk>
2023-04-26 10:19:50 +0100

    submission Bite 219 @ codechalleng.es

--- 219/notifications.py
+++ 219/notifications.py
@@ -4,4 +4,13 @@ TODAY = date.today()
 
 
 def gen_bite_planning(num_bites=1, num_days=1, start_date=TODAY):
-    pass
+    delta_days = timedelta(days=num_days)
+    bite_date = start_date + delta_days
+    bite_count = 0
+
+    while True:
+        if bite_count == num_bites:
+            bite_count = 0
+            bite_date += delta_days
+        yield bite_date
+        bite_count += 1
