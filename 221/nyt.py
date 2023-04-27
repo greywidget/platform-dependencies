f79fd1cb58d57cbfc4c6e6c84b2f05a419f71b37
Craig Richards <craig@greywidget.co.uk>
2023-04-27 13:56:37 +0100

    submission Bite 221 @ codechalleng.es

--- 221/nyt.py
+++ 221/nyt.py
@@ -1,31 +1,43 @@
+from operator import itemgetter
 import requests
 
-YOUR_KEY = '123abc'
-DEFAULT_LIST = 'hardcover-nonfiction'
+# from decouple import config
 
-URL_NON_FICTION = (f'https://api.nytimes.com/svc/books/v3/lists/current/'
-                   f'{DEFAULT_LIST}.json?api-key={YOUR_KEY}')
-URL_FICTION = URL_NON_FICTION.replace('nonfiction', 'fiction')
+# YOUR_KEY = config("API_KEY")
+YOUR_KEY = "123abc"
+DEFAULT_LIST = "hardcover-nonfiction"
+
+URL_NON_FICTION = (
+    f"https://api.nytimes.com/svc/books/v3/lists/current/"
+    f"{DEFAULT_LIST}.json?api-key={YOUR_KEY}"
+)
+URL_FICTION = URL_NON_FICTION.replace("nonfiction", "fiction")
 
 
 def get_best_seller_titles(url=URL_NON_FICTION):
     """Use the NY Times Books API endpoint above to get the titles that are
-       on the best seller list for the longest time.
+    on the best seller list for the longest time.
 
-       Return a list of (title, weeks_on_list) tuples, e.g. for the nonfiction:
+    Return a list of (title, weeks_on_list) tuples, e.g. for the nonfiction:
 
-       [('BETWEEN THE WORLD AND ME', 86),
-        ('EDUCATED', 79),
-        ('BECOMING', 41),
-        ('THE SECOND MOUNTAIN', 18),
-         ... 11 more ...
-       ]
+    [('BETWEEN THE WORLD AND ME', 86),
+     ('EDUCATED', 79),
+     ('BECOMING', 41),
+     ('THE SECOND MOUNTAIN', 18),
+      ... 11 more ...
+    ]
 
-       Dev docs: https://developer.nytimes.com/docs/books-product/1/overview
+    Dev docs: https://developer.nytimes.com/docs/books-product/1/overview
     """
-    pass
+    all_books = requests.get(url).json()["results"]["books"]
+    data = [(book["title"], book["weeks_on_list"]) for book in all_books]
+    return sorted(data, key=itemgetter(1), reverse=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    from rich import inspect, print
+    from rich.traceback import install
+
+    install(show_locals=True)
     ret = get_best_seller_titles()
     print(ret)
