cf4c3a790638f8817bf45f7686d7876828d024bc
Craig Richards <craig@greywidget.co.uk>
2023-05-03 14:34:58 +0100

    submission Bite 228 @ codechalleng.es

--- 228/gravatar.py
+++ 228/gravatar.py
@@ -1,17 +1,20 @@
 import hashlib
 
-GRAVATAR_URL = ("https://www.gravatar.com/avatar/"
-                "{hashed_email}?s={size}&r=g&d=robohash")
+GRAVATAR_URL = (
+    "https://www.gravatar.com/avatar/" "{hashed_email}?s={size}&r=g&d=robohash"
+)
 
 
 def create_gravatar_url(email, size=200):
     """Use GRAVATAR_URL above to create a gravatar URL.
 
-       You need to create a hash of the email passed in.
+    You need to create a hash of the email passed in.
 
-       PHP example: https://en.gravatar.com/site/implement/hash/
+    PHP example: https://en.gravatar.com/site/implement/hash/
 
-       For Python check hashlib check out (md5 / hexdigest):
-       https://docs.python.org/3/library/hashlib.html#hashlib.hash.hexdigest
+    For Python check hashlib check out (md5 / hexdigest):
+    https://docs.python.org/3/library/hashlib.html#hashlib.hash.hexdigest
     """
-    pass
+    hashed_email = hashlib.md5(email.lower().strip().encode()).hexdigest()
+
+    return GRAVATAR_URL.format(hashed_email=hashed_email, size=size)
