0ce6806cee7877dbeb0d6570b6a46e49028e7d8d
Craig Richards <craig@greywidget.co.uk>
2023-11-19 12:31:45 +0000

    submission Bite 373 @ codechalleng.es

--- 373/reverse_letters.py
+++ 373/reverse_letters.py
@@ -1,5 +1,16 @@
+from string import ascii_letters
+
+
 def reverse_letters(string: str) -> str:
     """Reverse letters in a string but keep the order of the non-letters the same"""
+    letters_only = [char for char in string if char in ascii_letters]
+    result = []
+    for char in string:
+        if char in ascii_letters:
+            result.append(letters_only.pop())
+        else:
+            result.append(char)
+    return "".join(result)
 
 
 if __name__ == "__main__":
