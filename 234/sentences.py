7b3db47172a92633d0e92a0b4778741d96e3856a
Craig Richards <craig@greywidget.co.uk>
2023-11-27 17:19:24 +0000

    submission Bite 234 @ codechalleng.es

--- 234/sentences.py
+++ 234/sentences.py
@@ -1,4 +1,10 @@
+import re
+
+RE_SENTENCE = re.compile(r"[^.?!]+(\.|!|\?)\s*")
+
+
 def capitalize_sentences(text: str) -> str:
     """Return text capitalizing the sentences. Note that sentences can end
-       in dot (.), question mark (?) and exclamation mark (!)"""
-    pass
+    in dot (.), question mark (?) and exclamation mark (!)"""
+    sentences = [match.group().capitalize() for match in re.finditer(RE_SENTENCE, text)]
+    return "".join(sentences)
