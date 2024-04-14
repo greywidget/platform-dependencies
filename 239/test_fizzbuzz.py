00b7953b84e9932a2524405845d74b243e24feae
Craig Richards <craig@greywidget.co.uk>
2024-04-14 17:22:22 +0100

    submission Bite 239 @ codechalleng.es

--- 239/test_fizzbuzz.py
+++ 239/test_fizzbuzz.py
@@ -1,3 +1,17 @@
 from fizzbuzz import fizzbuzz
 
-# write one or more pytest functions below, they need to start with test_
+
+def test_divisible_by_3_and_5():
+    assert fizzbuzz(15) == "Fizz Buzz"
+
+
+def test_divisible_by_3_only():
+    assert fizzbuzz(6) == "Fizz"
+
+
+def test_divisible_by_5_only():
+    assert fizzbuzz(35) == "Buzz"
+
+
+def test_divisible_by_neither():
+    assert fizzbuzz(88) == 88
