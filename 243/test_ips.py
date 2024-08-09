8301ab8aa1cd0c810cdecea2c0be966273bd4eec
Craig Richards <craig@greywidget.co.uk>
2024-08-09 16:24:10 +0100

    submission Bite 243 @ codechalleng.es

--- 243/test_ips.py
+++ 243/test_ips.py
@@ -10,8 +10,7 @@ from ips import ServiceIPRange, parse_ipv4_service_ranges, get_aws_service_range
 URL = "https://bites-data.s3.us-east-2.amazonaws.com/ip-ranges.json"
 TMP = os.getenv("TMP", "/tmp")
 PATH = Path(TMP, "ip-ranges.json")
-# IP = IPv4Network("192.0.2.8")
-IP = "54.244.46.0"
+IP = IPv4Network("192.0.2.8/29")
 
 
 @pytest.fixture(scope="module")
@@ -21,20 +20,42 @@ def json_file():
     return PATH
 
 
-def test_ServiceIPRange():
-    an_ip_range = ServiceIPRange("AMAZON", "us-east-1", "52.96.245.0/24")
-    assert (
-        str(an_ip_range)
-        == "52.96.245.0/24 is allocated to the AMAZON service in the us-east-1 region"
+def test_dataclass():
+    expected = "192.0.2.8/29 is allocated to the pybites service " "in the US region"
+    assert str(ServiceIPRange("pybites", "US", IP)) == expected
+
+
+def test_parse_ranges(json_file):
+    out = parse_ipv4_service_ranges(json_file)
+    assert len(out) == 1886
+
+    assert type(out) == list
+    assert all(type(element) == ServiceIPRange for element in out)
+    assert str(out[0]) == (
+        "13.248.118.0/24 is allocated to the AMAZON " "service in the eu-west-1 region"
     )
 
 
-def test_parser(json_file):
-    # breakpoint()
-    ip_ranges = parse_ipv4_service_ranges(source=json_file)
-    aws_ranges = get_aws_service_range(IP, ip_ranges)
-    an_ip_range = ServiceIPRange("AMAZON", "us-west-2", IPv4Network("54.244.0.0/16"))
-    assert an_ip_range in aws_ranges
+def test_get_aws_service_range_zero_hits(json_file):
+    assert get_aws_service_range("13.248.118.0", []) == []
 
-    with pytest.raises(ValueError, match="Address must be a valid IPv4 address"):
-        aws_ranges = get_aws_service_range("IP", ip_ranges)
+
+def test_get_aws_service_range_two_hits(json_file):
+    service_range = parse_ipv4_service_ranges(json_file)
+    expected = [
+        ServiceIPRange(
+            service="AMAZON", region="eu-west-1", cidr=IPv4Network("13.248.118.0/24")
+        ),
+        ServiceIPRange(
+            service="GLOBALACCELERATOR",
+            region="eu-west-1",
+            cidr=IPv4Network("13.248.118.0/24"),
+        ),
+    ]
+    assert get_aws_service_range("13.248.118.0", service_range) == expected
+
+
+def test_get_aws_service_range_exception(json_file):
+    with pytest.raises(ValueError) as exc:
+        get_aws_service_range("nonsense", {})
+    assert str(exc.value) == "Address must be a valid IPv4 address"
