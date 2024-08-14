8088cc188c8c9d239c640b2425b885e8ed35a73b
Craig Richards <craig@greywidget.co.uk>
2024-08-14 18:31:21 +0100

    submission Bite 245 @ codechalleng.es

--- 245/improved_xmas_tree.py
+++ 245/improved_xmas_tree.py
@@ -5,6 +5,17 @@ TRUNK = "|"
 
 def generate_improved_xmas_tree(rows=10):
     """Generate a xmas tree with a star (+), leafs (*) and a trunk (|)
-       for given rows of leafs (default 10).
-       For more information see the test and the bite description"""
-    pass
+    for given rows of leafs (default 10).
+    For more information see the test and the bite description"""
+
+    max_width = 2 * (rows - 1) + 1
+    lines = [f"{STAR:^{max_width}}"]
+
+    for width in range(rows):
+        lines.append(f"{LEAF * (width * 2 + 1):^{max_width}}")
+        trunk_width = round(max_width / 2) + 1
+
+    for _ in range(2):
+        lines.append(f"{TRUNK * trunk_width:^{max_width}}")
+
+    return "\n".join(lines)
