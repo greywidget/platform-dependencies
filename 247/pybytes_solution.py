1355a95acaa8405a38e675baafbd65c5c0727096
Craig Richards <craig@greywidget.co.uk>
2024-08-15 11:26:54 +0100

    Added PyBites solution to bite 247 for reference

---
+++ 247/pybytes_solution.py
@@ -0,0 +1,15 @@
+from unittest.mock import patch
+
+import color
+import pytest
+
+
+@pytest.fixture(scope="module")
+def gen():
+    return color.gen_hex_color()
+
+
+@patch.object(color, "sample", side_effect=[[191, 165, 216], [101, 102, 103]])
+def test_gen_hex_color(mock, gen):
+    assert next(gen) == "#BFA5D8"
+    assert next(gen) == "#656667"
