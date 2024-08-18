48ba5f55346e61b51dde75c90ccc9fd4e6073b66
Craig Richards <craig@greywidget.co.uk>
2024-08-18 18:28:29 +0100

    submission Bite 358 @ codechalleng.es

--- 358/test_script.py
+++ 358/test_script.py
@@ -1,9 +1,14 @@
 import pytest
-from typer.testing import CliRunner
-
 from script import app
+from typer.testing import CliRunner
 
 
 @pytest.fixture()
 def runner() -> CliRunner:
     return CliRunner()
+
+
+def test_app(runner):
+    result = runner.invoke(app, "Boris")
+    assert result.exit_code == 0
+    assert "Hello Boris" in result.stdout
