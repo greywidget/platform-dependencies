55c7a86aba06f67bb43bc652360735b708fb006b
Craig Richards <craig@greywidget.co.uk>
2024-08-18 18:37:56 +0100

    submission Bite 358 @ codechalleng.es

--- 358/test_script.py
+++ 358/test_script.py
@@ -12,3 +12,9 @@ def test_app(runner):
     result = runner.invoke(app, "Boris")
     assert result.exit_code == 0
     assert "Hello Boris" in result.stdout
+
+
+def test_help(runner):
+    result = runner.invoke(app, "--help")
+    assert result.exit_code == 0
+    assert "The name of the person to greet" in result.stdout
