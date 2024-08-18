940db44b513ff2ffffd18bab49d0d27ab53cc613
Craig Richards <craig@greywidget.co.uk>
2024-08-18 17:27:49 +0100

    submission Bite 354 @ codechalleng.es

--- 354/script.py
+++ 354/script.py
@@ -1,4 +1,5 @@
 import typer
+from typing_extensions import Annotated
 
 
 def sum_numbers(a: int, b: int):
@@ -8,10 +9,17 @@ def sum_numbers(a: int, b: int):
 def main(
     a: int = typer.Argument(..., help="The value of the first summand"),
     b: int = typer.Argument(..., help="The value of the second summand"),
+    c: Annotated[int, typer.Option(help="Optional third value.")] = None,
 ):
     """CLI that allows you to add two numbers"""
-
-    print(sum_numbers(a, b))
+    a_plus_b = sum_numbers(a, b)
+    output = f"The sum is {a_plus_b} "
+    if c:
+        comparison = "is smaller" if c < a_plus_b else "is not smaller"
+        output += f"and c {comparison}"
+    else:
+        output += "and c is None"
+    print(output)
 
 
 if __name__ == "__main__":
