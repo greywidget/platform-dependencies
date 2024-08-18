f44a0dd19e0c829483e1222e2db232c5c83697e0
Craig Richards <craig@greywidget.co.uk>
2024-08-18 19:08:20 +0100

    submission Bite 361 @ codechalleng.es

--- 361/script.py
+++ 361/script.py
@@ -1,11 +1,22 @@
 import typer
-
+from rich.console import Console
+from rich.table import Column, Table
 
 app = typer.Typer()
 
+console = Console()
 
+
+@app.command()
 def table():
-    pass
+    table = Table(
+        Column(header="Name"),
+        Column(header="Favorite Tool/Framework"),
+    )
+    table.add_row("Bob", "Vim")
+    table.add_row("Julian", "Flask")
+    table.add_row("Robin", "VS Code")
+    console.print(table)
 
 
 if __name__ == "__main__":
