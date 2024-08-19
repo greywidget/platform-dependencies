419c98dc73586544734347e63b5b8e8839eee203
Craig Richards <craig@greywidget.co.uk>
2024-08-19 16:44:07 +0100

    submission Bite 362 @ codechalleng.es

--- 362/script.py
+++ 362/script.py
@@ -1,5 +1,6 @@
 import typer
-from typing_extensions import Annotated
+
+# from typing_extensions import Annotated
 
 app = typer.Typer()
 
@@ -7,9 +8,9 @@ app = typer.Typer()
 @app.command()
 def main(
     name: str,
-    password: Annotated[
-        str, typer.Option(..., prompt=True, confirmation_prompt=True, hide_input=True)
-    ],
+    password: str = typer.Option(
+        ..., prompt=True, confirmation_prompt=True, hide_input=True
+    ),
 ):
     print(f"Hello {name}. Doing something very secure with password.")
     print(f"...just kidding, here it is, very insecure: {password}")
