2d542cb80b9c867d18d2c46324536f8b64f2da98
Craig Richards <craig@greywidget.co.uk>
2024-08-19 16:32:57 +0100

    submission Bite 362 @ codechalleng.es

--- 362/script.py
+++ 362/script.py
@@ -8,7 +8,7 @@ app = typer.Typer()
 def main(
     name: str,
     password: Annotated[
-        str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)
+        str, typer.Option(..., prompt=True, confirmation_prompt=True, hide_input=True)
     ],
 ):
     print(f"Hello {name}. Doing something very secure with password.")
