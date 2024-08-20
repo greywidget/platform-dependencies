dc2dae9f0eaeb5656441633947ee07dbc851ef37
Craig Richards <craig@greywidget.co.uk>
2024-08-20 17:46:53 +0100

    submission Bite 171 @ codechalleng.es

--- 171/animation.py
+++ 171/animation.py
@@ -1,17 +1,20 @@
 from itertools import cycle
-import sys
-from time import time, sleep
+from time import sleep
 
-SPINNER_STATES = ['-', '\\', '|', '/']  # had to escape \
+SPINNER_STATES = ["-", "\\", "|", "/"]  # had to escape \
 STATE_TRANSITION_TIME = 0.1
 
 
 def spinner(seconds):
     """Make a terminal loader/spinner animation using the imports above.
-       Takes seconds argument = time for the spinner to run.
-       Does not return anything, only prints to stdout."""
-    pass
+    Takes seconds argument = time for the spinner to run.
+    Does not return anything, only prints to stdout."""
+    states = cycle(SPINNER_STATES)
+    animation_count = int(round(seconds / STATE_TRANSITION_TIME))
+    for _ in range(animation_count):
+        print(next(states), end="\r", flush=True)
+        sleep(STATE_TRANSITION_TIME)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     spinner(2)
